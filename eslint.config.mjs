38503bd0fd46fe28faae04e914ced1fdad1d1023
developerlucaslima <developerlucaslima@gmail.com>
2025-04-30 01:37:59 -0300

    style: update import statements and formatting in eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,21 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { FlatCompat } from '@eslint/eslintrc'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    '@rocketseat/eslint-config/next',
+  ),
 
   {
     plugins: {
@@ -22,6 +26,6 @@ const eslintConfig = [
       'simple-import-sort/exports': 'error',
     },
   },
-];
+]
 
-export default eslintConfig;
+export default eslintConfig
