24303027b1883b5989ef89237fa2976eaaba29de
developerlucaslima <developerlucaslima@gmail.com>
2025-04-30 21:39:46 -0300

    refactor: restructure buttonVariants for improved readability and maintainability; add ButtonText component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,20 +5,40 @@ import * as React from 'react'
 import { cn } from '@/utils/cn'
 
 const buttonVariants = cva(
-  "inline-flex items-center cursor-pointer justify-center leading-[24px] text-base whitespace-nowrap font-medium transition-all rounded-sm shrink-0 outline-none transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 aria-invalid:border-destructive",
+  [
+    // Layout
+    'inline-flex items-center justify-center whitespace-nowrap rounded-sm shrink-0',
+    // Spacing
+    'leading-[24px] text-base tracking-[0.5px]',
+    // Borders
+    'border-2',
+    // Typography
+    'font-medium',
+    // Behavior
+    'cursor-pointer transition-all outline-none',
+    // Disabled
+    'disabled:pointer-events-none disabled:opacity-50',
+    // Focus
+    'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
+    // Validation (aria)
+    'aria-invalid:ring-destructive/20 aria-invalid:border-destructive',
+    // SVG handling
+    '[&_svg]:pointer-events-none [&_svg:not([class*="size-"])]:size-4 [&_svg]:shrink-0',
+  ],
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        outline: 'border-foreground border-2 bg-background hover:bg-accent',
+        default:
+          'bg-primary border-primary text-primary-foreground hover:bg-primary/80 hover:border-transparent',
         secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent',
+          'bg-secondary border-secondary text-secondary-foreground hover:bg-secondary/80 hover:border-transparent',
+        outline: 'border-foreground bg-background hover:bg-accent',
+        ghost: 'hover:bg-accent border-transparent',
+        link: 'text-link border-transparent hover:text-link/80',
       },
       size: {
-        default: 'px-7 py-3 has-[>svg]:px-3',
-        // sm: 'gap-1.5 has-[>svg]:px-2.5',
-        // lg: 'px-6 has-[>svg]:px-4',
+        default: 'p-3 has-[>svg]:px-2',
+        lg: 'p-3 md:p-5 md:text-2xl md:font-bold md:px-12 has-[>svg]:px-2',
         icon: 'size-9',
       },
     },
@@ -50,4 +70,8 @@ function Button({
   )
 }
 
-export { Button, buttonVariants }
+function ButtonText({ className, ...props }: React.ComponentProps<'span'>) {
+  return <span className={cn('px-4 sm:px-6', className)} {...props} />
+}
+
+export { Button, ButtonText, buttonVariants }
