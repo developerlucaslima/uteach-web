2d13c55ef0624b95e806d599139f971bab05436f
developerlucaslima <developerlucaslima@gmail.com>
2025-05-05 15:10:31 -0300

    feat: update Home component to include Header, HeroSection, and AboutSection for improved layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${roboto.variable} container mx-auto overflow-hidden antialiased`}
-      >
+      <body className={`${roboto.variable} container mx-auto antialiased`}>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import { Header } from '@components/header/header'
+
+import { AboutSection } from '@/sections/2-about-section/about-section'
+import { HeroSection } from '@/sections/hero-section/hero-section'
+
 export default function Home() {
   return (
-    <h1>UTeach</h1>
-  );
+    <>
+      <Header />
+      <HeroSection />
+      <AboutSection />
+    </>
+  )
 }
